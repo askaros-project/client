c1b0028df8e5cfd4b79720e64723acd1e01f09cc
oleg <s2ferr@gmail.com>
2019-02-06 11:26:41 +0300

    callback page

---
+++ pages/callback/twitter.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+ </div>
+</template>
+
+<script>
+export default {
+	name: "TwitterCallbackPage",
+	mounted() {
+		if (this.$mobx.account.isLoggedIn) {
+			this.$router.push('/')
+			return
+		}
+		this.$mobx.account.loginByTwitter({
+			oauth_token: this.$route.query.oauth_token,
+			oauth_verifier: this.$route.query.oauth_verifier
+		})
+	}
+}
+</script>
+
+<style lang="less" scoped></style>
