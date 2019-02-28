eb06ed96850f91309014c580691fd3959ccf99e6
oleg <s2ferr@gmail.com>
2019-02-28 11:17:45 +0300

    redirect to https

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const port = process.env.PORT || 3000
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 
 app.set('port', port)
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
+
+if (!config.dev) {
+  app.use(redirectToHTTPS())
+}
 
 async function start() {
   // Init Nuxt.js
